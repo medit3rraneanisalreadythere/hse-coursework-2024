fdc9d5036301863e6ca783333f97d6b81c5aa72c
Ivan K <1kononenko1@list.ru>
2024-03-24 19:17:54 +0300

    Initial Commit

---
+++ coursework-2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coursework-2024
+//
+//  Created by Таня Белка on 24.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
